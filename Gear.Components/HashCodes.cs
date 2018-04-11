72ff59afdc1284beccc8edbc0fb4fac33c1b5a37
Daniel Henry <daniel.henry@epiforge.com>
2018-04-10 20:03:17 -0400

    Components: HashCodes

---
+++ Gear.Components/HashCodes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Gear.Components
+{
+    public static class HashCodes
+    {
+        /// <summary>
+        /// Combines multiple hash codes into a single hash code
+        /// </summary>
+        /// <param name="hashCodes">The hash codes to combine</param>
+        /// <returns>The combined hash code</returns>
+        public static int CombineHashCodes(params int[] hashCodes) => CombineMultiple(hashCodes);
+
+        static int CombineMultiple(IEnumerable<int> hashCodes)
+        {
+            var enumerator = hashCodes.GetEnumerator();
+            if (!enumerator.MoveNext())
+                return 0;
+            var combined = enumerator.Current;
+            while (enumerator.MoveNext())
+                combined = CombineTwoHashCodes(combined, enumerator.Current);
+            return combined;
+        }
+
+        /// <summary>
+        /// Gets the hash codes for multiple objects and produces a single combined hash code
+        /// </summary>
+        /// <param name="objects">The objects for which to get hash codes</param>
+        /// <returns>The combined hash code</returns>
+        public static int CombineObjects(params object[] objects) => CombineMultiple(objects.Select(o => o?.GetHashCode() ?? 0));
+
+        static int CombineTwoHashCodes(int h1, int h2) => ((int)(((uint)h1 << 5) | ((uint)h1 >> 27)) + h1) ^ h2;
+    }
+}
