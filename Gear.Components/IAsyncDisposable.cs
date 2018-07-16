0670f3cde0d55aec5358171ed823ba42bce25a54
Daniel Henry <daniel.henry@epiforge.com>
2018-07-16 14:29:04 -0400

    Components: Whoops, forgot the actual file; and to note this is a NuGet release

---
+++ Gear.Components/IAsyncDisposable.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gear.Components
+{
+    public interface IAsyncDisposable
+    {
+        Task DisposeAsync(CancellationToken cancellationToken = default);
+    }
+}
