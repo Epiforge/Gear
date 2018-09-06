fe7876f856bb25bce73bbeab083d6b90311e9ade
Daniel Henry <daniel.henry@epiforge.com>
2018-09-06 16:21:09 -0400

    Components: added EquatableList; NuGet release

---
+++ Gear.Components/EquatableList.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gear.Components
+{
+    struct EquatableList<T> : IReadOnlyList<T>, IEquatable<EquatableList<T>>
+    {
+        public static bool operator ==(EquatableList<T> a, EquatableList<T> b) => a.Equals(b);
+
+        public static bool operator !=(EquatableList<T> a, EquatableList<T> b) => !a.Equals(b);
+
+        public EquatableList(IReadOnlyList<T> elements)
+        {
+            this.elements = elements;
+            hashCode = HashCodes.CombineObjects(this.elements);
+        }
+
+        readonly IReadOnlyList<T> elements;
+        readonly int hashCode;
+
+        public override bool Equals(object obj) => obj is EquatableList<T> other ? Equals(other) : false;
+
+        public bool Equals(EquatableList<T> other) => other != null && elements.SequenceEqual(other.elements);
+
+        public IEnumerator<T> GetEnumerator() => elements.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => elements.GetEnumerator();
+
+        public override int GetHashCode() => hashCode;
+
+        public T this[int index] => elements[index];
+
+        public int Count => elements.Count;
+    }
+}
