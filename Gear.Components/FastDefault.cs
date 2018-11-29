ed3d52295ed9f168b9e680599e22290d27ad762d
Daniel Henry <daniel.henry@epiforge.com>
2018-11-29 16:20:50 -0500

    Components: fixed a defect in FastDefault; NuGet release

--- Gear.Components/FastDefault.cs
+++ Gear.Components/FastDefault.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Concurrent;
+using System.Linq;
 using System.Reflection;
 
 namespace Gear.Components
@@ -7,7 +8,7 @@ namespace Gear.Components
     public static class FastDefault
     {
         static readonly ConcurrentDictionary<Type, object> defaults = new ConcurrentDictionary<Type, object>();
-        static readonly MethodInfo getDefaultMethod = typeof(FastDefault).GetRuntimeMethod(nameof(GetDefault), new Type[0]);
+        static readonly MethodInfo getDefaultMethod = typeof(FastDefault).GetRuntimeMethods().Single(method => method.Name == nameof(GetDefault));
 
         static object CreateDefault(Type type) => getDefaultMethod.MakeGenericMethod(type).Invoke(null, null);
 
