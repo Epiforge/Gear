3a344141acde741d60685f64723b9bd8c276d90f
Daniel Henry <daniel.henry@epiforge.com>
2018-12-19 14:16:47 -0500

    ActiveQuery: added ExpressionlessEmptyNonNotifier, ExpressionlessEmptySource, ExpressionlessNonNotifier, ExpressionlessSourceManipulation, SourceManipulation unit tests for Enumerable ActiveFirst

---
+++ Gear.ActiveQuery.MSTest/Enumerable/ActiveFirst.cs
@@ -0,0 +1,81 @@
+using System;
+using Gear.Components;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gear.ActiveQuery.MSTest.Enumerable
+{
+    [TestClass]
+    public class ActiveFirst
+    {
+        [TestMethod]
+        public void ExpressionlessEmptySource()
+        {
+            var numbers = new RangeObservableCollection<int>();
+            using (var query = numbers.ActiveFirst())
+            {
+                Assert.IsNotNull(query.OperationFault);
+                Assert.AreEqual(0, query.Value);
+            }
+        }
+
+        [TestMethod]
+        public void ExpressionlessEmptyNonNotifier()
+        {
+            var numbers = System.Linq.Enumerable.Empty<int>();
+            using (var query = numbers.ActiveFirst())
+            {
+                Assert.IsNotNull(query.OperationFault);
+                Assert.AreEqual(0, query.Value);
+            }
+        }
+
+        [TestMethod]
+        public void ExpressionlessNonNotifier()
+        {
+            var numbers = System.Linq.Enumerable.Range(0, 10);
+            using (var query = numbers.ActiveFirst())
+            {
+                Assert.IsNull(query.OperationFault);
+                Assert.AreEqual(0, query.Value);
+            }
+        }
+
+        [TestMethod]
+        public void ExpressionlessSourceManipulation()
+        {
+            var numbers = new RangeObservableCollection<int>(System.Linq.Enumerable.Range(0, 10));
+            using (var query = numbers.ActiveFirst())
+            {
+                Assert.IsNull(query.OperationFault);
+                Assert.AreEqual(0, query.Value);
+                numbers.Remove(0);
+                Assert.AreEqual(1, query.Value);
+                numbers.Clear();
+                Assert.IsNotNull(query.OperationFault);
+                Assert.AreEqual(0, query.Value);
+                numbers.Add(30);
+                Assert.IsNull(query.OperationFault);
+                Assert.AreEqual(30, query.Value);
+            }
+        }
+
+        [TestMethod]
+        public void SourceManipulation()
+        {
+            var numbers = new RangeObservableCollection<int>(System.Linq.Enumerable.Range(0, 10));
+            using (var query = numbers.ActiveFirst(i => i % 3 == 0))
+            {
+                Assert.IsNull(query.OperationFault);
+                Assert.AreEqual(0, query.Value);
+                numbers.Remove(0);
+                Assert.AreEqual(3, query.Value);
+                numbers.RemoveAll(i => i % 3 == 0);
+                Assert.IsNotNull(query.OperationFault);
+                Assert.AreEqual(0, query.Value);
+                numbers.Add(30);
+                Assert.IsNull(query.OperationFault);
+                Assert.AreEqual(30, query.Value);
+            }
+        }
+    }
+}
