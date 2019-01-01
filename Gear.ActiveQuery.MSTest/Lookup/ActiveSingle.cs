a9b3c528284227c6f32c9ae06cb11843b85855f0
Daniel Henry <daniel.henry@epiforge.com>
2019-01-01 12:56:46 -0500

    ActiveQuery: fixed a coverage lapse in Enumerable ActiveSingle SourceManipulation; added unit tests for Dictionary ActiveSingle

--- Gear.ActiveQuery.MSTest/Lookup/ActiveSingle.cs
+++ Gear.ActiveQuery.MSTest/Lookup/ActiveSingle.cs
@@ -1,8 +1,9 @@
 using Gear.Components;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.Linq;
 
-namespace Gear.ActiveQuery.MSTest.Enumerable
+namespace Gear.ActiveQuery.MSTest.Lookup
 {
     [TestClass]
     public class ActiveSingle
@@ -10,130 +11,130 @@ namespace Gear.ActiveQuery.MSTest.Enumerable
         [TestMethod]
         public void EmptySource()
         {
-            var numbers = new RangeObservableCollection<int>();
-            using (var expr = numbers.ActiveSingle(i => i % 3 == 0))
+            var numbers = new ObservableDictionary<int, int>();
+            using (var expr = numbers.ActiveSingle((key, value) => value % 3 == 0))
             {
                 Assert.IsNotNull(expr.OperationFault);
-                Assert.AreEqual(0, expr.Value);
+                Assert.AreEqual(0, expr.Value.Value);
             }
         }
 
         [TestMethod]
         public void ExpressionlessEmptyNonNotifier()
         {
-            var numbers = new int[0];
+            var numbers = new Dictionary<int, int>();
             using (var expr = numbers.ActiveSingle())
             {
                 Assert.IsNotNull(expr.OperationFault);
-                Assert.AreEqual(0, expr.Value);
+                Assert.AreEqual(0, expr.Value.Value);
             }
         }
 
         [TestMethod]
         public void ExpressionlessEmptySource()
         {
-            var numbers = new RangeObservableCollection<int>();
+            var numbers = new ObservableDictionary<int, int>();
             using (var expr = numbers.ActiveSingle())
             {
                 Assert.IsNotNull(expr.OperationFault);
-                Assert.AreEqual(0, expr.Value);
+                Assert.AreEqual(0, expr.Value.Value);
             }
         }
 
         [TestMethod]
         public void ExpressionlessMultiple()
         {
-            var numbers = new RangeObservableCollection<int>(new int[] { 1, 1 });
+            var numbers = new ObservableDictionary<int, int>(System.Linq.Enumerable.Range(1, 2).ToDictionary(i => i));
             using (var expr = numbers.ActiveSingle())
             {
                 Assert.IsNotNull(expr.OperationFault);
-                Assert.AreEqual(0, expr.Value);
+                Assert.AreEqual(0, expr.Value.Value);
             }
         }
 
         [TestMethod]
         public void ExpressionlessNonNotifier()
         {
-            var numbers = new int[] { 1 };
+            var numbers = System.Linq.Enumerable.Range(1, 1).ToDictionary(i => i);
             using (var expr = numbers.ActiveSingle())
             {
                 Assert.IsNull(expr.OperationFault);
-                Assert.AreEqual(1, expr.Value);
+                Assert.AreEqual(1, expr.Value.Value);
             }
         }
 
         [TestMethod]
         public void ExpressionlessNonNotifierMultiple()
         {
-            var numbers = new int[] { 1, 1 };
+            var numbers = System.Linq.Enumerable.Range(1, 2).ToDictionary(i => i);
             using (var expr = numbers.ActiveSingle())
             {
                 Assert.IsNotNull(expr.OperationFault);
-                Assert.AreEqual(0, expr.Value);
+                Assert.AreEqual(0, expr.Value.Value);
             }
         }
 
         [TestMethod]
         public void ExpressionlessSourceManipulation()
         {
-            var numbers = new RangeObservableCollection<int>(new int[] { 1 });
+            var numbers = new ObservableDictionary<int, int>(System.Linq.Enumerable.Range(1, 1).ToDictionary(i => i));
             using (var expr = numbers.ActiveSingle())
             {
                 Assert.IsNull(expr.OperationFault);
-                Assert.AreEqual(1, expr.Value);
-                numbers.Add(2);
+                Assert.AreEqual(1, expr.Value.Value);
+                numbers.Add(2, 2);
                 Assert.IsNotNull(expr.OperationFault);
-                Assert.AreEqual(0, expr.Value);
-                numbers.RemoveAt(0);
+                Assert.AreEqual(0, expr.Value.Value);
+                numbers.Remove(1);
                 Assert.IsNull(expr.OperationFault);
-                Assert.AreEqual(2, expr.Value);
+                Assert.AreEqual(2, expr.Value.Value);
                 numbers.Clear();
                 Assert.IsNotNull(expr.OperationFault);
-                Assert.AreEqual(0, expr.Value);
+                Assert.AreEqual(0, expr.Value.Value);
             }
         }
 
         [TestMethod]
         public void Multiple()
         {
-            var numbers = new RangeObservableCollection<int>(System.Linq.Enumerable.Range(1, 3).Select(i => i * 3));
-            using (var expr = numbers.ActiveSingle(i => i % 3 == 0))
+            var numbers = new ObservableDictionary<int, int>(System.Linq.Enumerable.Range(1, 3).ToDictionary(i => i, i => i * 3));
+            using (var expr = numbers.ActiveSingle((key, value) => value % 3 == 0))
             {
                 Assert.IsNotNull(expr.OperationFault);
-                Assert.AreEqual(0, expr.Value);
+                Assert.AreEqual(0, expr.Value.Value);
             }
         }
 
         [TestMethod]
         public void SourceManipulation()
         {
-            var numbers = new RangeObservableCollection<int>(System.Linq.Enumerable.Range(1, 3));
-            using (var expr = numbers.ActiveSingle(i => i % 3 == 0))
+            var numbers = new ObservableDictionary<int, int>(System.Linq.Enumerable.Range(1, 3).ToDictionary(i => i));
+            using (var expr = numbers.ActiveSingle((key, value) => value % 3 == 0))
             {
                 Assert.IsNull(expr.OperationFault);
-                Assert.AreEqual(3, expr.Value);
-                numbers.RemoveAt(2);
+                Assert.AreEqual(3, expr.Value.Value);
+                numbers.Remove(3);
                 Assert.IsNotNull(expr.OperationFault);
-                Assert.AreEqual(0, expr.Value);
-                numbers.Add(3);
+                Assert.AreEqual(0, expr.Value.Value);
+                numbers.Add(3, 3);
                 Assert.IsNull(expr.OperationFault);
-                Assert.AreEqual(3, expr.Value);
-                numbers.Add(5);
+                Assert.AreEqual(3, expr.Value.Value);
+                numbers.Add(5, 5);
                 Assert.IsNull(expr.OperationFault);
-                Assert.AreEqual(3, expr.Value);
-                numbers.Add(6);
+                Assert.AreEqual(3, expr.Value.Value);
+                numbers.Add(6, 6);
                 Assert.IsNotNull(expr.OperationFault);
-                Assert.AreEqual(0, expr.Value);
+                Assert.AreEqual(0, expr.Value.Value);
                 numbers.Clear();
                 Assert.IsNotNull(expr.OperationFault);
-                Assert.AreEqual(0, expr.Value);
-                numbers.Add(3);
-                numbers.Add(6);
+                Assert.AreEqual(0, expr.Value.Value);
+                numbers.Add(3, 3);
+                numbers.Add(6, 6);
                 Assert.IsNotNull(expr.OperationFault);
-                Assert.AreEqual(0, expr.Value);
-                numbers.RemoveAt(0);
+                Assert.AreEqual(0, expr.Value.Value);
+                numbers.Remove(3);
                 Assert.IsNull(expr.OperationFault);
-                Assert.AreEqual(6, expr.Value);
+                Assert.AreEqual(6, expr.Value.Value);
             }
         }
     }
