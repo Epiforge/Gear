548774bc0073bd14da631755b13d2dbd6e6e710d
Daniel Henry <daniel.henry@epiforge.com>
2019-04-02 17:24:43 -0400

    Components: added reference to Microsoft TPL Dataflow NuGet; AsyncSynchronizationContext revised to rely upon BufferBlock from Microsoft TPL Dataflow (which is much better than the sillyness it replaced); NuGet release

--- Gear.Components/AsyncSynchronizationContext.cs
+++ Gear.Components/AsyncSynchronizationContext.cs
@@ -1,63 +1,88 @@
-using Nito.AsyncEx;
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Runtime.ExceptionServices;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace Gear.Components
 {
     /// <summary>
     /// Provides a synchronization context for the Task Parallel Library
     /// </summary>
-    public class AsyncSynchronizationContext : SynchronizationContext
+    public class AsyncSynchronizationContext : SynchronizationContext, IDisposable
     {
-        readonly AsyncLock queuedCallbacksExecutionAccess = new AsyncLock();
-        readonly ConcurrentQueue<(SendOrPostCallback callback, object state, ManualResetEventSlim signal, Exception exception)> queuedCallbacks = new ConcurrentQueue<(SendOrPostCallback callback, object state, ManualResetEventSlim signal, Exception exception)>();
-
-        void ExecuteQueuedCallbacks() => Task.Run(async () =>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncSynchronizationContext"/> class
+        /// </summary>
+        public AsyncSynchronizationContext()
         {
-            using (await queuedCallbacksExecutionAccess.LockAsync().ConfigureAwait(false))
+            queuedCallbacksCancellationTokenSource = new CancellationTokenSource();
+            queuedCallbacks = new BufferBlock<(SendOrPostCallback callback, object state, ManualResetEventSlim signal, Exception exception)>(new DataflowBlockOptions { CancellationToken = queuedCallbacksCancellationTokenSource.Token });
+            Task.Run(ProcessCallbacks);
+        }
+
+        internal AsyncSynchronizationContext(bool allowDisposal) : this() =>
+            this.allowDisposal = allowDisposal;
+
+        /// <summary>
+        /// Finalizes this object
+        /// </summary>
+        ~AsyncSynchronizationContext() => Dispose(false);
+
+        readonly bool allowDisposal;
+        readonly BufferBlock<(SendOrPostCallback callback, object state, ManualResetEventSlim signal, Exception exception)> queuedCallbacks;
+        readonly CancellationTokenSource queuedCallbacksCancellationTokenSource;
+
+        async Task ProcessCallbacks()
+        {
+            while (true)
             {
-                if (!queuedCallbacks.IsEmpty)
+                var csse = await queuedCallbacks.ReceiveAsync().ConfigureAwait(false);
+                var currentContext = Current;
+                SetSynchronizationContext(this);
+                var (callback, state, signal, _) = csse;
+                try
                 {
-                    var postedCallbackExceptions = new List<Exception>();
-                    var currentContext = Current;
-                    SetSynchronizationContext(this);
-                    while (queuedCallbacks.TryDequeue(out var csse))
-                    {
-                        var (callback, state, signal, _) = csse;
-                        try
-                        {
-                            callback(state);
-                        }
-                        catch (Exception ex)
-                        {
-                            csse.exception = ex;
-                        }
-                        if (signal != null)
-                            signal.Set();
-                        else if (csse.exception != null)
-                            postedCallbackExceptions.Add(csse.exception);
-                    }
-                    SetSynchronizationContext(currentContext);
-                    if (postedCallbackExceptions.Count > 0)
-                        throw new AggregateException("Unhandled exceptions encountered by posted callbacks", postedCallbackExceptions);
+                    callback(state);
                 }
+                catch (Exception ex)
+                {
+                    csse.exception = ex;
+                }
+                if (signal != null)
+                    signal.Set();
+                SetSynchronizationContext(currentContext);
             }
-        });
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources
+        /// </summary>
+        public void Dispose()
+        {
+            if (!allowDisposal)
+                throw new InvalidOperationException();
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Frees, releases, or resets unmanaged resources
+        /// </summary>
+        /// <param name="disposing"><c>false</c> if invoked by the finalizer because the object is being garbage collected; otherwise, <c>true</c></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                queuedCallbacksCancellationTokenSource.Cancel();
+        }
 
         /// <summary>
         /// Dispatches an asynchronous message to the synchronization context
         /// </summary>
         /// <param name="d">The <see cref="SendOrPostCallback"/> delegate to call</param>
         /// <param name="state">The object passed to the delegate</param>
-        public override void Post(SendOrPostCallback d, object state)
-        {
-            queuedCallbacks.Enqueue((d ?? throw new ArgumentNullException(nameof(d)), state, null, null));
-            ExecuteQueuedCallbacks();
-        }
+        public override void Post(SendOrPostCallback d, object state) =>
+            queuedCallbacks.Post((d ?? throw new ArgumentNullException(nameof(d)), state, null, null));
 
         /// <summary>
         /// Dispatches a synchronous message to the synchronization context
@@ -69,8 +94,7 @@ namespace Gear.Components
             using (var signal = new ManualResetEventSlim(false))
             {
                 var csse = (callback: d ?? throw new ArgumentNullException(nameof(d)), state, signal, exception: (Exception)null);
-                queuedCallbacks.Enqueue(csse);
-                ExecuteQueuedCallbacks();
+                queuedCallbacks.Post(csse);
                 signal.Wait();
                 if (csse.exception != null)
                     ExceptionDispatchInfo.Capture(csse.exception).Throw();
--- Gear.Components/Synchronization.cs
+++ Gear.Components/Synchronization.cs
@@ -7,7 +7,7 @@ namespace Gear.Components
     /// </summary>
     public static class Synchronization
     {
-        static readonly AsyncSynchronizationContext asyncSynchronizationContext = new AsyncSynchronizationContext();
+        static readonly AsyncSynchronizationContext asyncSynchronizationContext = new AsyncSynchronizationContext(false);
 
         /// <summary>
         /// Gets the synchronization context that is used by methods in this library that require one when <see cref="SynchronizationContext.Current"/> is <c>null</c>
