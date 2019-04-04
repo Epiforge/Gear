fc5c4d2d9b2179f7d9d8cbb09b3af0a30c675c83
Daniel Henry <daniel.henry@epiforge.com>
2019-04-04 01:36:51 -0400

    ActiveQuery: fixed breaking changes from Components

--- Gear.ActiveQuery/IActiveEnumerable.cs
+++ Gear.ActiveQuery/IActiveEnumerable.cs
@@ -10,7 +10,7 @@ namespace Gear.ActiveQuery
     /// Represents a read-only collection of elements that is the result of an active query
     /// </summary>
     /// <typeparam name="TElement">The type of the elements in the sequence</typeparam>
-    public interface IActiveEnumerable<out TElement> : IDisposable, IDisposeStatus, INotifyCollectionChanged, INotifyDisposed, INotifyDisposing, INotifyElementFaultChanges, INotifyGenericCollectionChanged<TElement>, INotifyPropertyChanged, INotifyPropertyChanging, IReadOnlyList<TElement>, ISynchronized
+    public interface IActiveEnumerable<out TElement> : IDisposable, IDisposalStatus, INotifyCollectionChanged, INotifyDisposed, INotifyDisposing, INotifyElementFaultChanges, INotifyGenericCollectionChanged<TElement>, INotifyPropertyChanged, INotifyPropertyChanging, IReadOnlyList<TElement>, ISynchronized
     {
     }
 }
